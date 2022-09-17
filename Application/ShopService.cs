8a2e33be4b9376f62d7392b85c057a15e50e029e
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-09-17 10:52:51 +0200

    Changed logic of sortByDistance to check to errors first

--- Application/ShopService.cs
+++ Application/ShopService.cs
@@ -9,16 +9,16 @@ public class ShopService : IShopService
 {
     public List<Entities.Shop> SortByDistance(List<Entities.Shop> source, GeoCoordinate y)
     {
+        if (source == null || y == null)
+            throw new NullReferenceException("Something went wrong, try again later.");
+        if (y.Latitude < -90.0 && y.Longitude < -90.0 && y.Latitude > 90.0 && y.Longitude > 90.0)
+            throw new ArgumentException("Your location is unknown, try a different location.");
+        if (source.Count <= 0)
+            throw new ArgumentException("Unable to find shops near your location.");
         
-
-        if (y.Latitude >= -90.0 && y.Longitude >= -90.0 && y.Latitude <= 90.0 && y.Longitude <= 90.0)
-        { 
-            return source.OrderBy(x => new GeoCoordinate(x._gpsLocation.Longitude, x._gpsLocation.Latitude).GetDistanceTo(new GeoCoordinate(y.Longitude, y.Latitude))).ToList();
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
+        
+        
+        return source.OrderBy(x => new GeoCoordinate(x._gpsLocation.Longitude, x._gpsLocation.Latitude).GetDistanceTo(new GeoCoordinate(y.Longitude, y.Latitude))).ToList();
     }
 
     public List<Entities.Shop> FilterByLocation(List<Entities.Shop> source, GeoCoordinate x, int width, int height)
