74c98d6fe07030a58d507e6c3ebb7dc72f385853
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-09-17 11:41:06 +0200

    sortByDistance test suit and service done.

--- Application/ShopService.cs
+++ Application/ShopService.cs
@@ -11,14 +11,26 @@ public class ShopService : IShopService
     {
         if (source == null || y == null)
             throw new NullReferenceException("Something went wrong, try again later.");
-        if (y.Latitude < -90.0 && y.Longitude < -90.0 && y.Latitude > 90.0 && y.Longitude > 90.0)
-            throw new ArgumentException("Your location is unknown, try a different location.");
+        if (y.Latitude < -45.0 || y.Longitude < -45.0 || y.Latitude > 45.0 || y.Longitude > 45.0)
+            throw new ArgumentOutOfRangeException("Your location is unknown, try a different location.");
         if (source.Count <= 0)
             throw new ArgumentException("Unable to find shops near your location.");
         
         
+        var shopsNearby = source.OrderBy(x => new GeoCoordinate(x._gpsLocation.Longitude, x._gpsLocation.Latitude).GetDistanceTo(new GeoCoordinate(y.Longitude, y.Latitude))).ToList();
+
+        foreach (var shop in shopsNearby.ToList())
+        {
+            var g = shop._gpsLocation;
+            
+            //If the shops in the list is outside the specified range of the plane, remove them from the list.
+            if (g.Latitude < -45.0 || g.Longitude < -45.0 || g.Latitude > 45.0 || g.Longitude > 45.0)
+            {
+                shopsNearby.Remove(shop);
+            }
+        }
         
-        return source.OrderBy(x => new GeoCoordinate(x._gpsLocation.Longitude, x._gpsLocation.Latitude).GetDistanceTo(new GeoCoordinate(y.Longitude, y.Latitude))).ToList();
+        return shopsNearby;
     }
 
     public List<Entities.Shop> FilterByLocation(List<Entities.Shop> source, GeoCoordinate x, int width, int height)
--- ShopServiceTest/ShopServiceTest.cs
+++ ShopServiceTest/ShopServiceTest.cs
@@ -29,7 +29,67 @@ public class UnitTest1
         // Assert
         Assert.Equal(ShopName, actual.ElementAt(index).Name);
     }
+    
+    [Theory]
+    [InlineData(-46.0, 0.0, "Your location is unknown, try a different location.")]
+    [InlineData(0.0, 46.0, "Your location is unknown, try a different location.")]
+    [InlineData(-45.1, 45.1, "Your location is unknown, try a different location.")]
+    public void TestSortByDistanceInvalidData_ClientOutOfTheBoundsOfThePlane(double latitude, double longitude, string errorMsg)
+    {
+        // Arrange
+        List<Entities.Shop> source = SupplyValidShops();
+        GeoCoordinate geoCoordinate = new GeoCoordinate(latitude, longitude);
 
+        // Act + Assert
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => _shopService.SortByDistance(source, geoCoordinate));
+        Assert.Equal(errorMsg, ex.ParamName);
+    }
+    
+    [Fact]
+    public void TestSortByDistance_ValidAndInvalidShops()
+    {
+        // Arrange
+        var theMixedList = new List<Entities.Shop>();
+        var geoLocation = new GeoCoordinate(0.0, 0.0);
+        var invalidShops = SupplyInvalidShops();
+        var validShops = SupplyValidShops();
+        
+        for (int i = 0; i < 9; i++)
+        {
+            theMixedList.Add(invalidShops.ElementAt(i));
+            theMixedList.Add(validShops.ElementAt(i));
+        }
+        
+        // Act
+        var result = _shopService.SortByDistance(theMixedList, geoLocation);
+
+        //Assert
+        Assert.Equal(validShops.Count,result.Count);
+    }
+    
+    [Fact]
+    public void TestSortByDistance_NullVallues()
+    {
+        // Arrange
+        string errorMsg = "Something went wrong, try again later.";
+
+        // Act + Assert
+        var ex = Assert.Throws<NullReferenceException>(() => _shopService.SortByDistance(null, null));
+        Assert.Equal(errorMsg, ex.Message);
+    }
+    
+    [Fact]
+    public void TestSortByDistance_EmptyListOfShops()
+    {
+        // Arrange
+        var source = new List<Entities.Shop>();
+        var geoLocation = new GeoCoordinate(0.0, 0.0);
+        string errorMsg = "Unable to find shops near your location.";
+
+        // Act + Assert
+        var ex = Assert.Throws<ArgumentException>(() => _shopService.SortByDistance(source, geoLocation));
+        Assert.Equal(errorMsg, ex.Message);
+    }
 
 
     static List<Entities.Shop> SupplyValidShops()
@@ -37,14 +97,14 @@ public class UnitTest1
         return new List<Entities.Shop>()
         {
             new Entities.Shop(){Name = "Pet World", website = "Petworld.dk", Address = "H.C Andersensvej 23", _gpsLocation = new Entities.GPSLocation(){Latitude = 0.0, Longitude = 0.0}},
-            new Entities.Shop(){Name = "Bilka", website = "Bilka.dk", Address = "H.C Andersensvej 12", _gpsLocation = new Entities.GPSLocation(){Latitude = -80.0, Longitude = -90.0}},
-            new Entities.Shop(){Name = "Black Rose", website = "Black-rose.dk", Address = "H.C Andersensvej 13", _gpsLocation = new Entities.GPSLocation(){Latitude = -90.0, Longitude = 80.0}},
-            new Entities.Shop(){Name = "Normal", website = "Normal.dk", Address = "H.C Andersensvej 223", _gpsLocation = new Entities.GPSLocation(){Latitude = 75.0, Longitude = -90.0}},
-            new Entities.Shop(){Name = "EASV", website = "EASV.dk", Address = "H.C Andersensvej 123", _gpsLocation = new Entities.GPSLocation(){Latitude = 90.0, Longitude = 90.0}},
-            new Entities.Shop(){Name = "Støvsugeposer", website = "Støvsugeposer.dk", Address = "H.C Andersensvej 3", _gpsLocation = new Entities.GPSLocation(){Latitude = 45.0, Longitude = 2.0}},
-            new Entities.Shop(){Name = "Handy Mand John Johnson", website = "Handymandesbjerg.dk", Address = "H.C Andersensvej 212", _gpsLocation = new Entities.GPSLocation(){Latitude = -10.0, Longitude = -10.0}},
-            new Entities.Shop(){Name = "Fitness World", website = "Fitnessworld.dk", Address = "H.C Andersensvej 2323", _gpsLocation = new Entities.GPSLocation(){Latitude = -35.0, Longitude = 15.0}},
-            new Entities.Shop(){Name = "Gamestop", website = "Gamestop.dk", Address = "H.C Andersensvej 2223", _gpsLocation = new Entities.GPSLocation(){Latitude = 15.0, Longitude = 15.0}},
+            new Entities.Shop(){Name = "Bilka", website = "Bilka.dk", Address = "H.C Andersensvej 12", _gpsLocation = new Entities.GPSLocation(){Latitude = -40.0, Longitude = -45.0}},
+            new Entities.Shop(){Name = "Black Rose", website = "Black-rose.dk", Address = "H.C Andersensvej 13", _gpsLocation = new Entities.GPSLocation(){Latitude = -45.0, Longitude = 40.0}},
+            new Entities.Shop(){Name = "Normal", website = "Normal.dk", Address = "H.C Andersensvej 223", _gpsLocation = new Entities.GPSLocation(){Latitude = 34.5, Longitude = -45.0}},
+            new Entities.Shop(){Name = "EASV", website = "EASV.dk", Address = "H.C Andersensvej 123", _gpsLocation = new Entities.GPSLocation(){Latitude = 45.0, Longitude = 45.0}},
+            new Entities.Shop(){Name = "Støvsugeposer", website = "Støvsugeposer.dk", Address = "H.C Andersensvej 3", _gpsLocation = new Entities.GPSLocation(){Latitude = 22.5, Longitude = 1.0}},
+            new Entities.Shop(){Name = "Handy Mand John Johnson", website = "Handymandesbjerg.dk", Address = "H.C Andersensvej 212", _gpsLocation = new Entities.GPSLocation(){Latitude = -5.0, Longitude = -5.0}},
+            new Entities.Shop(){Name = "Fitness World", website = "Fitnessworld.dk", Address = "H.C Andersensvej 2323", _gpsLocation = new Entities.GPSLocation(){Latitude = -17.5, Longitude = 7.5}},
+            new Entities.Shop(){Name = "Gamestop", website = "Gamestop.dk", Address = "H.C Andersensvej 2223", _gpsLocation = new Entities.GPSLocation(){Latitude = 7.5, Longitude = 7.5}},
         };
         
         
@@ -54,15 +114,15 @@ public class UnitTest1
     {
        return new List<Entities.Shop>()
         {
-            new Entities.Shop(){Name = "Pet World", website = "Petworld.dk", Address = "H.C Andersensvej 23", _gpsLocation = new Entities.GPSLocation(){Latitude = 0.0, Longitude = 0.0}},
-            new Entities.Shop(){Name = "Bilka", website = "Bilka.dk", Address = "H.C Andersensvej 12", _gpsLocation = new Entities.GPSLocation(){Latitude = -91.0, Longitude = -90.0}},
-            new Entities.Shop(){Name = "Black Rose", website = "Black-rose.dk", Address = "H.C Andersensvej 13", _gpsLocation = new Entities.GPSLocation(){Latitude = -90.0, Longitude = 91.0}},
-            new Entities.Shop(){Name = "Normal", website = "Normal.dk", Address = "H.C Andersensvej 223", _gpsLocation = new Entities.GPSLocation(){Latitude = 90.0, Longitude = -90.0}},
-            new Entities.Shop(){Name = "EASV", website = "EASV.dk", Address = "H.C Andersensvej 123", _gpsLocation = new Entities.GPSLocation(){Latitude = 92.0, Longitude = 90.0}},
-            new Entities.Shop(){Name = "Støvsugeposer", website = "Støvsugeposer.dk", Address = "H.C Andersensvej 3", _gpsLocation = new Entities.GPSLocation(){Latitude = 45.0, Longitude = 2.0}},
-            new Entities.Shop(){Name = "Handy Mand John Johnson", website = "Handymandesbjerg.dk", Address = "H.C Andersensvej 212", _gpsLocation = new Entities.GPSLocation(){Latitude = -10.0, Longitude = -10.0}},
-            new Entities.Shop(){Name = "Fitness World", website = "Fitnessworld.dk", Address = "H.C Andersensvej 2323", _gpsLocation = new Entities.GPSLocation(){Latitude = -35.0, Longitude = 15.0}},
-            new Entities.Shop(){Name = "Gamestop", website = "Gamestop.dk", Address = "H.C Andersensvej 2223", _gpsLocation = new Entities.GPSLocation(){Latitude = 95.0, Longitude = 15.0}},
+            new Entities.Shop(){Name = "Pet World", website = "Petworld.dk", Address = "H.C Andersensvej 23", _gpsLocation = new Entities.GPSLocation(){Latitude = 55.0, Longitude = 0.0}},
+            new Entities.Shop(){Name = "Bilka", website = "Bilka.dk", Address = "H.C Andersensvej 12", _gpsLocation = new Entities.GPSLocation(){Latitude = -46.0, Longitude = -46.0}},
+            new Entities.Shop(){Name = "Black Rose", website = "Black-rose.dk", Address = "H.C Andersensvej 13", _gpsLocation = new Entities.GPSLocation(){Latitude = -46.0, Longitude = 46.0}},
+            new Entities.Shop(){Name = "Normal", website = "Normal.dk", Address = "H.C Andersensvej 223", _gpsLocation = new Entities.GPSLocation(){Latitude = 46.0, Longitude = -46.0}},
+            new Entities.Shop(){Name = "EASV", website = "EASV.dk", Address = "H.C Andersensvej 123", _gpsLocation = new Entities.GPSLocation(){Latitude = 46.0, Longitude = 46.0}},
+            new Entities.Shop(){Name = "Støvsugeposer", website = "Støvsugeposer.dk", Address = "H.C Andersensvej 3", _gpsLocation = new Entities.GPSLocation(){Latitude = 72.5, Longitude = 1.0}},
+            new Entities.Shop(){Name = "Handy Mand John Johnson", website = "Handymandesbjerg.dk", Address = "H.C Andersensvej 212", _gpsLocation = new Entities.GPSLocation(){Latitude = -50.0, Longitude = -5.0}},
+            new Entities.Shop(){Name = "Fitness World", website = "Fitnessworld.dk", Address = "H.C Andersensvej 2323", _gpsLocation = new Entities.GPSLocation(){Latitude = -17.5, Longitude = 75.5}},
+            new Entities.Shop(){Name = "Gamestop", website = "Gamestop.dk", Address = "H.C Andersensvej 2223", _gpsLocation = new Entities.GPSLocation(){Latitude = 57.5, Longitude = 7.5}},
         };
     }
 }
